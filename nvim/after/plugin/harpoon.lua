9966b3e0203c18dc3a7138f9b8b0e1f486c8ec98
Andy Park <chanhyup@andrew.cmu.edu>
2024-06-24 22:28:42 -0400

    Edit harpoon

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -3,7 +3,7 @@ local keymap = vim.keymap
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-keymap.set("n", "<leader>hm", mark.add_file, { desc = "Add current file to harpoon" })
+keymap.set("n", "<leader>ha", mark.add_file, { desc = "Add current file to harpoon" })
 keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Open up harpoon menu" })
 keymap.set("n", "<leader>hp", ui.nav_prev, { desc = "Go to previous harpoon mark" })
 
